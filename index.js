94c7282c4ac0120a7bd079e9e3a10e997e2eb844
Nikita <terencekity@gmail.com>
2023-08-20 15:53:34 -0400

    complete task

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(arr,name){
+    let result = []
+    for (let i = 0; i<arr.length ; i++){
+       result.push(`Thank you, ${arr[i]}, for the wonderful ${name} gift!`)
+    }
+    return result
+}
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"))
+function countDown (i){
+    while (i>=0){
+        console.log(i)
+        i--
+    }
+}
